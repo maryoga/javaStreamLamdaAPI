d321568c7f5d8372446065f8d899579d6a165bcd
maryogaJUG <maryoga@domain.com>
2023-09-17 09:37:58 -0600

    Prog Funcional Lamda y metodos a referencia

--- src/main/java/com/hope/App.java
+++ src/main/java/com/hope/App.java
@@ -11,12 +11,8 @@ import com.hope.model.Product;
  * Hello world!
  *
  */
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-
+public class App {
+    public static void main(String[] args) {
         Person p1 = new Person(1, "Quijote", LocalDate.of(1991, 1, 21));
         Person p2 = new Person(2, "Leroy", LocalDate.of(1999, 12, 3));
         Person p3 = new Person(3, "Tabita", LocalDate.of(2008, 4, 17));
@@ -27,15 +23,30 @@ public class App
         Product prod2 = new Product(2, "Trigo", 27.35);
         Product prod3 = new Product(3, "Maiz", 77.22);
         Product prod4 = new Product(4, "Soya", 45.00);
-        
-        List<Person> persons = Arrays.asList(p1, p2, p3, p4, p5);
-        List<Product> products = Arrays.asList(prod1, prod2, prod3, prod4);
 
-        // Lambda //method reference
-        // list.forEach(System.out::println);
-        for(int i=0; i < persons.size(); i++ ){
-            System.out.println(persons.get(i));
-        }
+        List < Person > persons = Arrays.asList(p1, p2, p3, p4, p5);
+        List < Product > products = Arrays.asList(prod1, prod2, prod3, prod4);
+
+        // Lambda method reference
+        // list.forEach(System.out::println)p
+        // Programacion imperativa;
+        //for (int i=0; i < persons.size(); i++ ) {
+        //    System.out.println(persons.get(i));
+        //}   
+
+
+        //for (Person p: persons){
+        //    System.out.println(p);
+        //}
+
+        // programacion funcional
+        //persons.forEach(p -> System.out.println(p));
+
+        // metodos a referencia
+        persons.forEach(System.out::println);
+
+
 
     }
 }
+
